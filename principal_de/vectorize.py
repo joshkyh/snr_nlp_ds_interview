953ae2553dbbd981123175c2e933b102ecd41b66
Joshua Kim <joshkyh@users.noreply.github.com>
2024-10-22 03:02:07 +0000

    Feature: Use devcontainers

--- principal_de/vectorize.py
+++ principal_de/vectorize.py
@@ -6,3 +6,6 @@ def vectorize(input_str:str) -> np.array:
     model = SentenceTransformer('BAAI/bge-small-en')
     embeddings_1 = model.encode(input_str, normalize_embeddings=True)
     return embeddings_1
+
+if __name__ == "__main__":
+    print(vectorize("hello"))
